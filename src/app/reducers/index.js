0d85e9abd104e524e8ecb1d6692e442754cbda37
nicolasmartello <nmartello@makingsense.com>
2018-04-27 14:26:07 -0300

    Combine list and favorites reducers in a single one

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,8 +1,68 @@
-import { combineReducers } from 'redux';
-import list from './list';
-import favorites from './favorites';
+import {
+  LOAD_GIF_LIST,
+  UPDATE_GIF_LIST,
+  ADD_ITEM_TO_FAVORITES,
+  REMOVE_ITEM_FROM_FAVORITES,
+  LOAD_FAVORITES_LIST,
+} from '../constants/actionTypes';
 
-export default combineReducers({
-  list,
-  favorites,
-});
+let arrFavorites = [];
+const initialState = {
+  list: {
+    result: [],
+  },
+  favorites: {
+    list: [],
+  }
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case LOAD_GIF_LIST:
+    if (state.favorites) {
+      arrFavorites = state.favorites.list;
+    } else {
+      arrFavorites = [];
+    }
+    return {
+      ...state,
+      list: {
+        result: action.gifList.map(item => ({
+          ...item,
+          is_in_favorites: (arrFavorites.findIndex(e => (e.id === item.id)) > -1),
+        }))
+      },
+    };
+  case UPDATE_GIF_LIST:
+    const i = state.list.result.findIndex(item => item.id === action.item.id);
+    const newListResult = state.list.result;
+    
+    newListResult[i].is_in_favorites = !newListResult[i].is_in_favorites;
+    return {
+      ...state,
+      result: newListResult,
+    };
+  case LOAD_FAVORITES_LIST:
+    return { ...state, favorites: { list: action.favorites } };
+  case ADD_ITEM_TO_FAVORITES:
+    if (!state.favorites.list) return state;
+    
+    const newFavoriteList = state.favorites.list;
+    newFavoriteList.push({ ...action.item, is_in_favorites: true });
+    
+    return {
+      ...state,
+      favorites: { list: newFavoriteList },
+    };
+  case REMOVE_ITEM_FROM_FAVORITES:
+    return {
+      ...state,
+      favorites: { list: state.list.filter(item => item.id !== action.item.id) },
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default rootReducer;
